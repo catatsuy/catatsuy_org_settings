bf2ba7a7b53f1494b942f8fb5881d39b60529459
catatsuy <catatsuy@catatsuy.org>
2026-04-19 15:18:02 +0900

    use lego

---
+++ letsencrypt/cert_ecdsa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+EMAIL="catatsuy@catatsuy.org"
+#SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
+SERVER='https://acme-v02.api.letsencrypt.org/directory'
+
+/usr/local/bin/lego \
+  --accept-tos \
+  --email "$EMAIL" \
+  --server "$SERVER" \
+  --path /home/www/lego/ecdsa \
+  --dns route53 \
+  --key-type ec384 \
+  --domains catatsuy.org \
+  --domains www.catatsuy.org \
+  --domains blog.catatsuy.org \
+  --domains ech.catatsuy.org \
+  renew \
+  --dynamic \
+  --profile shortlived
+
+cat /home/www/lego/ecdsa/certificates/catatsuy.org.crt \
+    /home/www/lego/ecdsa/certificates/catatsuy.org.issuer.crt \
+    > /home/www/lego/ecdsa/certificates/catatsuy.org.fullchain.pem
+
+/usr/local/bin/lego \
+  --accept-tos \
+  --email "$EMAIL" \
+  --server "$SERVER" \
+  --path /home/www/lego/ecdsa \
+  --dns route53 \
+  --key-type ec384 \
+  --domains matw.co \
+  renew \
+  --dynamic \
+  --profile shortlived
+
+cat /home/www/lego/ecdsa/certificates/matw.co.crt \
+    /home/www/lego/ecdsa/certificates/matw.co.issuer.crt \
+    > /home/www/lego/ecdsa/certificates/matw.co.fullchain.pem
+
---
+++ letsencrypt/cert_rsa.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+EMAIL="catatsuy@catatsuy.org"
+#SERVER="https://acme-staging-v02.api.letsencrypt.org/directory"
+SERVER='https://acme-v02.api.letsencrypt.org/directory'
+
+/usr/local/bin/lego \
+  --accept-tos \
+  --email "$EMAIL" \
+  --server "$SERVER" \
+  --path /home/www/lego/rsa \
+  --dns route53 \
+  --key-type rsa2048 \
+  --domains catatsuy.org \
+  --domains www.catatsuy.org \
+  --domains blog.catatsuy.org \
+  --domains ech.catatsuy.org \
+  renew \
+  --dynamic \
+  --profile shortlived
+
+cat /home/www/lego/rsa/certificates/catatsuy.org.crt \
+    /home/www/lego/rsa/certificates/catatsuy.org.issuer.crt \
+    > /home/www/lego/rsa/certificates/catatsuy.org.fullchain.pem
+
+/usr/local/bin/lego \
+  --accept-tos \
+  --email "$EMAIL" \
+  --server "$SERVER" \
+  --path /home/www/lego/rsa \
+  --dns route53 \
+  --key-type rsa2048 \
+  --domains matw.co \
+  renew \
+  --dynamic \
+  --profile shortlived
+
+cat /home/www/lego/rsa/certificates/matw.co.crt \
+    /home/www/lego/rsa/certificates/matw.co.issuer.crt \
+    > /home/www/lego/rsa/certificates/matw.co.fullchain.pem
+
